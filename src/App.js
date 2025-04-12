872e869ad92b522485c92e80ad49ae9f555dc862
KalleFjader <kalle.fjader@gmail.com>
2025-04-12 16:59:14 +0300

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Topbar from './Topbar';
 import Home from './Home';
 import About from './About';
