a0ed34fd073e7281776658ea6d302b8a243d9356
KalleFjader <kalle.fjader@gmail.com>
2025-04-12 17:58:09 +0300

    mobileview

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,7 @@ function Home() {
     <div>
       <div className="main-content">
         <h1>Solar Water Solutions</h1>
-        <p>Kox-Service är ett finskt företag specialiserat på installation och underhåll av moderna reningsverk för både privatpersoner och företag. Med över 15 års erfarenhet i branschen erbjuder vi skräddarsydda lösningar som säkerställer renare vatten och en hållbar framtid.</p>
+        <p className='infotext'>Kox-Service är ett finskt företag specialiserat på installation och underhåll av moderna reningsverk för både privatpersoner och företag. Med över 15 års erfarenhet i branschen erbjuder vi skräddarsydda lösningar som säkerställer renare vatten och en hållbar framtid.</p>
         <img src={myphoto} alt="A description of the photo" className="main-image" />
       </div>
     </div>
