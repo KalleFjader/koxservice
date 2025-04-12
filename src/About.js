9575e111b8808f70bb5e6ae94b4637fd1fdf9d67
KalleFjader <kalle.fjader@gmail.com>
2025-04-12 17:04:03 +0300

    hashv2

--- src/About.js
+++ src/About.js
@@ -6,9 +6,9 @@ function About() {
         <h1>Om oss</h1>
         <p>Kox-Service är ett finskt företag specialiserat på installation och underhåll av moderna reningsverk för både privatpersoner och företag. Med över 15 års erfarenhet i branschen erbjuder vi skräddarsydda lösningar som säkerställer renare vatten och en hållbar framtid.</p>
         <div>
-          <img src={myphoto} alt="A description of the photo" className="placeholder-image" />
-          <img src={myphoto} alt="A description of the photo" className="placeholder-image" />
-          <img src={myphoto} alt="A description of the photo" className="placeholder-image" />
+          <img src={myphoto} alt="Bild på tidigare arbete" className="placeholder-image" />
+          <img src={myphoto} alt="Bild på tidigare arbete" className="placeholder-image" />
+          <img src={myphoto} alt="Bild på tidigare arbete" className="placeholder-image" />
         </div>
       
       </div>
